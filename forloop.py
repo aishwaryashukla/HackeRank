98c7ee6c235137aebdc2589307fdeff389485a58
aishwaryashukla <aishwarya.shukla@gmail.com>
2017-07-26 11:35:22 +0530

    adding a forloop and range function

---
+++ forloop.py
@@ -0,0 +1,31 @@
+"""
+Task 
+Read an integer . For all non-negative integers , print . See the sample for details.
+
+Input Format
+
+The first and only line contains the integer, .
+
+Constraints
+
+
+Output Format
+
+Print  lines, one corresponding to each .
+
+Sample Input
+
+5
+Sample Output
+
+0
+1
+4
+9
+16
+
+"""
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
