428de45fa566458d7764554d06cdd851f6e74913
aishwaryashukla <aishwarya.shukla@gmail.com>
2023-09-07 19:31:15 +0800

    adding new files

---
+++ mutualfund.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from mftool import Mftool
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+mf = Mftool()
+scheme_codes = mf.get_scheme_codes()
+
+print(scheme_codes)
+nav_data = mf.get_scheme_historical_nav_for_dates('120828', '01-01-2021', '30-08-2023')
+pp.pprint(nav_data['data'])
+#
+def get_best_return(nava_data):
+    average_returns = {}
+    for d in nava_data:
+
+        day = d['date'][:2]
+        # day = row['weekday']
+
+        close_price = float(d['nav'])
+        if day in average_returns:
+            average_returns[day].append(close_price)
+        else:
+            average_returns[day] = [close_price]
+
+    # Calculate the mean returns for each day
+    for day, prices in average_returns.items():
+        average_returns[day] = sum(prices) / len(prices)
+        # pp.pprint(f'day = {day} and sum(prices) = {sum(prices)} and len(prices) = {len(prices)} {average_returns[day]} \n')
+
+    # Find the day with the highest average returns
+    best_day = min(average_returns, key=average_returns.get)
+    pp.pprint(average_returns)
+    return best_day
+
+print(get_best_return(nava_data=nav_data['data']))
